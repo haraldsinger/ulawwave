67703082ee5d04d3eb2f3337fb9af44aafc9966a
Harald Singer <Harald.Singer@nuance.com>
2016-10-04 20:39:10 -0400

    change from module to package due to limitation in setuptools

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import setuptools
 
 setuptools.setup(
     name = 'ulawwave',
-    # packages = ['ulawwave'], # this must be the same as the name above
-    version = '0.1',
-    description = 'extension to 21.5. wave — Read and write WAV files to deal with ulaw and alaw',
+    packages = ['ulawwave',], # this must be the same as the name above
+    version = '0.1.1',
+    description = 'extension to the standard wave module to deal with g711 (ulaw) and alaw',
     author = 'Eric Woudenberg, Harald Singer',
     author_email = 'haraldgsinger@gmail.com',
     url = 'https://github.com/haraldsinger/ulawwave', # use the URL to the github repo
